2f1ae3db2b1764ddfafceae7a08e23adb50bb83f
siro53 <shusuke.shirotake@me.com>
2021-01-24 17:36:55 +0900

    rank, select, quantile実装

--- wavlet_tree/wavlet_tree.cpp
+++ wavlet_tree/wavlet_tree.cpp
@@ -13,6 +13,7 @@ using std::cerr;
 using std::cout;
 using std::endl;
 using std::ostream;
+using std::pair;
 using std::string;
 using std::vector;
 
@@ -93,21 +94,22 @@ class bit_vector {
         u32 k = i % S_SIZE;
         return (B[b_id] >> k & 1);
     }
-    // B[0, i)の中の1の個数を返す
-    u32 rank(const u32 i) {
+    // B[0, i)の中のb={0,1}の個数を返す
+    u32 rank(const u32 i, const int b) {
         u32 l_id = i / L_SIZE;
         u32 b_id = i % L_SIZE / S_SIZE;
         u32 s_id = i % S_SIZE;
 
         u16 bit = B[l_id * s_len + b_id] & ((1 << s_id) - 1);
-        return L[l_id] + S[l_id][b_id] + P(bit);
+        int res = L[l_id] + S[l_id][b_id] + P(bit);
+        return (b == 1 ? res : i - res);
     }
-    // Bの中で、先頭から見てi番目に出現した1のindexを返す
-    u32 select(const u32 i) {
+    // Bの中で、先頭から見てi番目に出現したb={0,1}のindexを返す
+    u32 select(const u32 i, const int b) {
         u32 ok = n, ng = 0;
         while(ok - ng > 1) {
             u32 mid = (ok + ng) / 2;
-            (rank(mid) >= i ? ok : ng) = mid;
+            (rank(mid, b) >= i ? ok : ng) = mid;
         }
         return ok;
     }
@@ -124,14 +126,15 @@ ostream &operator<<(ostream &os, bit_vector &bv) {
     return os;
 }
 
-template <int alp_size = 26> class wavlet_tree {
+template <int alp_size> class wavlet_tree {
   private:
     struct Node {
         int l, r;
         Node *ch[2];
+        Node *par;
         bit_vector bv;
         vector<int> dat;
-        Node() : l(0), r(0), ch{nullptr, nullptr} {}
+        Node() : l(0), r(0), ch{nullptr, nullptr}, par(nullptr) {}
     };
     int n;
     Node *root;
@@ -144,6 +147,13 @@ template <int alp_size = 26> class wavlet_tree {
         cerr << now->bv << endl;
         debug_print(now->ch[0]);
         debug_print(now->ch[1]);
+    }
+    bool is_leaf(Node *node) { return (node->r - node->l == 1); }
+    bool is_root(Node *node) { return (node == root); }
+    int left_or_right(Node *node, int x) {
+        int mid = (node->r + node->l) / 2;
+        if(node->l <= x && x < mid) return 0;
+        return 1;
     }
 
   public:
@@ -176,10 +186,12 @@ template <int alp_size = 26> class wavlet_tree {
             now->bv.build();
             if(mid - l > 0) {
                 now->ch[0] = new Node;
+                now->ch[0]->par = now;
                 self(self, l, mid, now->ch[0], L);
             }
             if(r - mid > 0) {
                 now->ch[1] = new Node;
+                now->ch[1]->par = now;
                 self(self, mid, r, now->ch[1], R);
             }
         };
@@ -189,23 +201,85 @@ template <int alp_size = 26> class wavlet_tree {
     int access(int i) {
         int res = -1;
         auto dfs = [&](auto self, Node *now, int p) -> void {
-            if(now->r - now->l == 1) {
+            if(is_leaf(now)) {
                 res = now->dat[0];
                 return;
             }
             int b = now->bv.access(p);
-            int np = now->bv.rank(p);
-            if(b == 0) np = p - np;
+            int np = now->bv.rank(p, b);
             self(self, now->ch[b], np);
         };
         dfs(dfs, root, i);
         return res;
     }
-    int rank(int c, int i) {}
+    int rank(int c, int i) {
+        int res = -1;
+        auto dfs = [&](auto self, Node *now, int p) -> void {
+            if(is_leaf(now)) {
+                res = p;
+                return;
+            }
+            int b = left_or_right(now, c);
+            int np = now->bv.rank(p, b);
+            self(self, now->ch[b], np);
+        };
+        dfs(dfs, root, i);
+        return res;
+    }
+    int select(int c, int i) {
+        Node *leaf;
+        auto get_leaf = [&](auto self, Node *now) -> void {
+            if(is_leaf(now)) {
+                leaf = now;
+                return;
+            }
+            int b = left_or_right(now, c);
+            self(self, now->ch[b]);
+        };
+        get_leaf(get_leaf, root);
+        int res = -1;
+        auto dfs = [&](auto self, Node *now, int p) -> void {
+            if(is_root(now)) {
+                res = p;
+                return;
+            }
+            int b = left_or_right(now->par, now->l);
+            int np = now->par->bv.select(p, b);
+            self(self, now->par, np);
+        };
+        dfs(dfs, leaf, i + 1);
+        return res - 1;
+    }
+    int qualtile(int s, int e, int rank) {
+        int res = -1;
+        auto dfs = [&](auto self, Node *now, int st, int en,
+                       int remain) -> void {
+            if(is_leaf(now)) {
+                res = now->l;
+                return;
+            }
+            int zerocnt = now->bv.rank(en, 0) - now->bv.rank(st, 0);
+            if(remain < zerocnt) {
+                int nxt_st = now->bv.rank(st, 0);
+                int nxt_en = now->bv.rank(en, 0);
+                self(self, now->ch[0], nxt_st, nxt_en, remain);
+            } else {
+                int nxt_st = now->bv.rank(st, 1);
+                int nxt_en = now->bv.rank(en, 1);
+                self(self, now->ch[1], nxt_st, nxt_en, remain - zerocnt);
+            }
+        };
+        dfs(dfs, root, s, e, rank);
+        return res;
+    }
+    vector<pair<int, int>> topk(int s, int e, int k) {}
 };
 
 int main() {
     vector<int> v = {3, 0, 2, 0, 1, 3, 0, 1, 2, 1, 3, 2};
     wavlet_tree<4> wav(v);
     for(int i = 0; i < int(v.size()); i++) debug(wav.access(i));
+    debug(wav.rank(0, 5));
+    debug(wav.select(2, 1));
+    debug(wav.qualtile(1, 5, 2));
 }
